265e16199bc608de5ab16c12c21228392d7338b1
wwwyyying <iwangying_919@163.com>
2020-06-30 11:13:37 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<!--
+ * @Description:
+ * @version:
+ * @Author: YoungW
+ * @Date: 2020-06-29 21:14:33
+ * @LastEditors: YoungW
+ * @LastEditTime: 2020-06-29 21:28:24
+-->
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        meituan-app
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ server/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: YoungW
+ * @Date: 2020-06-29 21:23:57
+ * @LastEditors: YoungW
+ * @LastEditTime: 2020-06-29 21:24:24
+ */ 
+import Koa from 'koa'
